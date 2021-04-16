b57a5c7ebaa5ad6c7717aee6a0960678b0a41f90
caiokb <caiokbrasil@gmail.com>
2021-04-16 11:53:09 -0300

    Função baisca de comparação implementada

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ Description: desenvolva um programa que, de acordo com as mãos de dois jogadore
 const CARTAS = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'] //array que define a ordem de valor das cartas
 const NAIPES = ['D', 'H', 'S', 'C']
 const RANKS = ["Carta Alta", "Um Par", "Dois Pares",
-    "Trinca", "/Straight", "/Flush", "Full House",
-    "Quadra", "/Straight Flush", "/Royal Flush"]
+    "Trinca", "Straight", "Flush", "Full House",
+    "Quadra", "Straight Flush", "Royal Flush"]
 
 
-let testHand = "1H 1H 2H 2H 2H"
+let testHand = "1S 1H 3H 3H 4D"
+let testHand2 = "1H 1D 2S 3S 4C"
 
 //let mJogador1 = document.getElementById("jg1")
 //let mJogador2 = document.getElementById("jg2")
@@ -20,6 +21,17 @@ let testHand = "1H 1H 2H 2H 2H"
 function comparePlayersHand(m1, m2) {
     let h1 = getPlayersHand(m1);
     let h2 = getPlayersHand(m2);
+
+    if(h1 > h2){
+        console.log("1 Wins")
+    }
+    else if(h1 < h2){
+        console.log( "2 Wins")
+    }
+    else{
+        console.log("Tie")
+    }
+
 }
 
 
@@ -29,15 +41,41 @@ function getPlayersHand(cartasJogador) {
     let numeros = cartas.map(n => n[0]).sort((a, b) => CARTAS.indexOf(a) - CARTAS.indexOf(b))//separa apenas os "indices" das cartas e ordena de acordo com a ordem de valor definida no array CARTAS
     let naipes = cartas.map(n => n[1]) //separar apenas os naipes
 
-    let flush = naipes.every(n => n === n[0]) //verificar se os naipes são todos iguais  
+    let flush = naipes.every(n => n === naipes[0]) //verificar se os naipes são todos iguais  
     let straight = numeros.every((num, i) => i === numeros.length - 1 || CARTAS.indexOf(num) < CARTAS.indexOf(numeros[i + 1])) //verificar se os números formam uma sequencia
 
-    console.log(checkForFullHouse(numeros))
+    if(straight && flush && numeros[0] === 'T')
+        return 9
+
+    if(straight && flush)
+        return 8
+    
+    if(checkForQuadra(numeros).isQuadra)
+        return 7
+    
+    if(checkForFullHouse(numeros).isFullHouse)
+        return 6
+    
+    if(flush)
+        return 5
+    
+    if(straight)
+        return 4
+    
+    if(checkForTrinca(numeros).isTrinca)
+        return 3
+    
+    if(checkForTwoPair(numeros).isTwoPair)
+        return 2
+    
+    if(checkForPair(numeros).isPair)
+        return 1
+
 }
 
 function checkForQuadra(numeros) {
     let quadra = numeros.slice(0, 4) // array para procurar uma sequencia de 4 numeros iguais
-    let isQuadra = quadra.every((num, i) => num === quadra[i + 1]) //variavel booleana
+    let isQuadra = quadra.every((num, i) => num === quadra[i + 1] || i === quadra.length - 1) //variavel booleana
     let numQuadra = quadra[2]
 
     if (!isQuadra) {
@@ -126,8 +164,7 @@ function checkForPair(numeros) {
     return { isPair, pair1 }
 }
 
-
-getPlayersHand(testHand)
+comparePlayersHand(testHand, testHand2)
 
 
 
