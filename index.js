d0e96d569ae61289c3321d6c90f798d55b9fd626
caiokb <caiokbrasil@gmail.com>
2021-04-20 11:13:44 -0300

    Redução de funções e limpeza de código

--- index.js
+++ index.js
@@ -10,29 +10,31 @@ const NAIPES = ['D', 'H', 'S', 'C']
 const RANKS = ["Highcard", "Pair", "Two Pairs", "Three of a Kind", "Straight", "Flush", "Full House",
     "Four of a Kind", "Straight Flush", "Royal Flush"]
 
-let hand1 = "1H 1H 3S 4H 5H"
-let hand2 = "3C 3D 3S 9S 9D"
+let hand1 = "2H 3H 4H 5H 6H"
+let hand2 = "TD JD QD KD AD"
+
+let playerCards //variavel que recebe de cada função o dado necessário para desempate
 
 function comparePlayersHand(m1, m2) {
 
     let h1 = getPlayersHand(m1);
     let h2 = getPlayersHand(m2);
-    console.log(RANKS[h1.hand] + " x " + RANKS[h2.hand])
-
+    console.log("Player 1 " + "(" + hand1 + ")" + " x " + "(" + hand2 + ")" + " Player 2")
+    console.log("           " + RANKS[h1.hand] + " x " + RANKS[h2.hand] + "\n")
     //loop para comparar as cartas e definir o vencedor
     if (h1.hand === h2.hand) {
         for (let i = (h1.card.length - 1); i >= 0; i--) {
             if (CARTAS.indexOf(h1.card[i]) > CARTAS.indexOf(h2.card[i])) {
-                return "1 Wins"
+                return "Player 1 Wins"
             }
             if (CARTAS.indexOf(h1.card[i]) < CARTAS.indexOf(h2.card[i])) {
-                return "2 Wins"
+                return "Player 2 Wins"
             }
             if (i === 0)
                 return "Draw"
         }
     }
-    return h1.hand > h2.hand ? "1 Wins" : "2 Wins"
+    return h1.hand > h2.hand ? "Player 1 Wins" : "Player 2 Wins"
 }
 
 function getPlayersHand(cartasJogador) {
@@ -50,11 +52,11 @@ function getPlayersHand(cartasJogador) {
     if (straight && flush)
         return { hand: 8, card: numeros[0] }
 
-    if (checkForQuadra(numeros).isQuadra)
-        return { hand: 7, card: numeros }
+    if (isFourOfAKind(numeros))
+        return { hand: 7, card: playerCards }
 
-    if (checkForFullHouse(numeros).isFullHouse)
-        return { hand: 6, card: checkForFullHouse(numeros).numTFullHouse }
+    if (isFullHouse(numeros))
+        return { hand: 6, card: playerCards }
 
     if (flush)
         return { hand: 5, card: numeros }
@@ -62,107 +64,76 @@ function getPlayersHand(cartasJogador) {
     if (straight)
         return { hand: 4, card: numeros[0] }
 
-    if (checkForTrinca(numeros).isTrinca)
-        return { hand: 3, card: checkForTrinca(numeros).numTrinca }
+    if (isThreeOfAKind(numeros))
+        return { hand: 3, card: playerCards }
 
-    if (checkForTwoPair(numeros).isTwoPair)
-        return { hand: 2, card: checkForTwoPair(numeros).result }
+    if (isTwoPair(numeros))
+        return { hand: 2, card: playerCards }
 
-    if (checkForPair(numeros).isPair)
-        return { hand: 1, card: checkForPair(numeros).result }
+    if (isPair(numeros))
+        return { hand: 1, card: playerCards }
 
     return { hand: 0, card: highcard }
 }
 
 //função para verificar se o jogador possui uma quadra
-function checkForQuadra(numeros) {
-    let quadra = numeros.slice(0, 4) // array para procurar uma sequencia de 4 numeros iguais
-    let isQuadra = quadra.every((num, i) => num === quadra[i + 1] || i === quadra.length - 1) //variavel booleana
-    let numQuadra = quadra[2]
-
-    if (!isQuadra) {
-        quadra = numeros.slice(1, 5)
-        isQuadra = quadra.every((num, i) => num === quadra[i + 1] || i === quadra.length - 1)
+function isFourOfAKind(numeros) {
+    if (numeros[0] === numeros[3] || numeros[1] === numeros[4]) {
+        playerCards = numeros[2]
+        return true
     }
-    return { isQuadra, numQuadra } //retorna um boolean e o numero da quadra para o caso de um empate
+    return false
 }
 
 //função para verificar se o jogador possui um full house
-function checkForFullHouse(numeros) {
-    let valuesTrinca = checkForTrinca(numeros)
-    let isFullHouse
-    let valuesPair
-    let array
-    let numTFullHouse = valuesTrinca.numTrinca
-
-    if (valuesTrinca.isTrinca) {
-        array = numeros.filter(num => num !== valuesTrinca.numTrinca)
-        valuesPair = checkForPair(array)
-        if (valuesPair.isPair)
-            isFullHouse = true
-        else
-            isFullHouse = false
+function isFullHouse(numeros) {
+    let a
+    if (numeros[0] === numeros[2] || numeros[1] === numeros[3] || numeros[2] === numeros[4]) {
+        a = numeros[2]
+        if (numeros.some((num, i) => (num === numeros[i + 1]) && num !== a)) {
+            playerCards = a
+            return true
+        }
     }
-    return { isFullHouse, numTFullHouse }
+    return false
 }
 
 //função para verificar se o jogador possui uma trinca
-function checkForTrinca(numeros) {
-    let trinca = numeros.slice(0, 3)
-    let isTrinca = trinca.every((num, i) => num === trinca[i + 1] || i === trinca.length - 1)
-    let numTrinca = numeros[2]
-
-    if (!isTrinca) {
-        trinca = numeros.slice(1, 4)
-        isTrinca = trinca.every((num, i) => num === trinca[i + 1] || i === trinca.length - 1)
+function isThreeOfAKind(numeros) {
+    if (numeros[0] === numeros[2] || numeros[1] === numeros[3] || numeros[2] === numeros[4]) {
+        playerCards = numeros[2]
+        return true
     }
-    if (!isTrinca) {
-        trinca = numeros.slice(2, 5)
-        isTrinca = trinca.every((num, i) => num === trinca[i + 1] || i === trinca.length - 1)
-    }
-    return { isTrinca, numTrinca }
-}
-
-//função para verificar se o jogador possui um par
-function checkForPair(numeros) {
-    let array = numeros
-    let pair1 = 0
-    let isPair = array.some(
-        function (num, i) {
-            if (num === array[i + 1]) {
-                pair1 = num
-                return true
-            }
-            return false
-        }
-    )
-    let result = array.filter((num, index) => array.indexOf(num) === index)
-    let aux = result[3]
-    result[3] = pair1
-    result[result.indexOf(pair1)] = aux
-
-    return { isPair, pair1, result }
+    return false
 }
 
 //função para verificar se o jogador possui dois pares
-function checkForTwoPair(numeros) {
-    let array = numeros
-    let result = array.filter((num, index) => array.indexOf(num) === index)
-    let isTwoPair = checkForPair(array).isPair
-    let pair1 = checkForPair(array).pair1
+function isTwoPair(numeros) {
+    let a, b
+    if (numeros.some((num, i) => num === numeros[i + 1] || i === numeros.length - 1)) {
+        a = numeros.filter((num, index) => numeros.indexOf(num) !== index)
+    }
+    if (a.length === 2) {
+        b = numeros.filter((num, i) => num !== a[0] && num !== a[1])
+        a.unshift(b[0])
+        playerCards = a
+        return true
+    }
+    return false
+}
 
-    array = array.filter(num => num !== pair1)
-    isTwoPair = checkForPair(array).isPair
-    pair2 = checkForPair(array).pair1
+//função para verificar se o jogador possui um par
+function isPair(numeros) {
+    let a, b
 
-    let aux = result[2]
-    result[2] = pair2
-    result[result.indexOf(pair2)] = aux
-    aux = result[1]
-    result[1] = pair1
-    result[result.indexOf(pair1)] = aux
-
-    return { isTwoPair, result }
+    if (numeros.some((num, i) => num === numeros[i + 1])) {
+        a = numeros.filter((num, index) => numeros.indexOf(num) !== index)
+        b = numeros.filter((num, i) => num !== a[0] && num !== a[1])
+        b.push(a[0])
+        playerCards = b
+        return true
+    }
+    return false
 }
 
 console.log(comparePlayersHand(hand1, hand2))
